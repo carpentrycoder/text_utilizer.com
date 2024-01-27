78e5d5d45a9867ea427764745abdbd0e3187f37e
Sutar <nikhilsutar621@gmail.com>
2024-01-28 01:27:16 +0530

    Update in views.py
    - removing unnecessary code
    - adding three new features
    - upper-case , newline remover, remove punctuations

--- textutills/textutils/textutils/views.py
+++ textutills/textutils/textutils/views.py
@@ -2,35 +2,37 @@
 from django.shortcuts import render
 
 # let's learn to lying pipeline
-
-link = "http://127.0.0.1:8000"
-char_counter_link = "http://127.0.0.1:8000/charcounter"
-remove_punc_link = "http://127.0.0.1:8000/removepunc"
-capitalize_link = "http://127.0.0.1:8000/capatilazed"
-newline_remover_link = "http://127.0.0.1:8000/newlinermvr"
-space_remover_link = "http://127.0.0.1:8000/spaceremover"
-
-
 def index(request):
     return render(request, 'index.html')
-
-
 def analyze(request):
     djtxt = request.GET.get('text', 'default')
     removepunc = request.GET.get('removepunc', 'off')  # Default value is 'off' if checkbox not checked
+    UPPERCASE = request.GET.get('UPPERCASE', 'off')
+    new_line_remover = request.GET.get('new_line_remover', 'off')
     print(removepunc)
     print(djtxt)
-
-    if removepunc == 'on':  # Check if checkbox is checked
+    if(removepunc == 'on'):  # Check if checkbox is checked
         punctuation_list = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
         analyzer = ""
         for char in djtxt:
             if char not in punctuation_list:
                 analyzer = analyzer + char
         params = {'purpose': 'Remove Punctuation', 'analyzed_txt': analyzer}
+
+    elif(UPPERCASE == 'on'):
+        analyzer = ""
+        for char in djtxt:
+            analyzer = analyzer + char.upper()
+        params = {'purpose': 'UPPERCASE TEXT', 'analyzed_txt': analyzer}
+
+    elif(new_line_remover == 'on'):
+        analyzer = ""
+        for char in djtxt:
+            if char != "\n":
+                analyzer = analyzer + char
+        params = {'purpose':'new_line_remover', 'analyzed_txt': analyzer}
+
     else:
         params = {'purpose': 'No Operation Selected', 'analyzed_txt': djtxt}
 
     return render(request, 'analyze.html', params)
-
-
