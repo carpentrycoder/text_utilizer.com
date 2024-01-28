2c8184b229b7074171f839bc491583b76cf7631c
Sutar <nikhilsutar621@gmail.com>
2024-01-29 00:19:30 +0530

    Enhancements: Checkbox Styling, Character Count, and Bug Fixes
    - Updated index.html to include checkboxes with modified font size.
    
    - Adjusted views.py to correctly handle character count option in the text analyzer.
    
    - Added font-size styling to checkboxes for improved readability.
    
    - Fixed a bug in views.py where the extra_space_remover checkbox was mistakenly using the new_line_remover value.

--- textutills/textutils/textutils/views.py
+++ textutills/textutils/textutils/views.py
@@ -9,8 +9,11 @@ def analyze(request):
     removepunc = request.GET.get('removepunc', 'off')  # Default value is 'off' if checkbox not checked
     UPPERCASE = request.GET.get('UPPERCASE', 'off')
     new_line_remover = request.GET.get('new_line_remover', 'off')
+    extra_space_remover = request.GET.get('new_line_remover', 'off')
+    char_count = request.GET.get('char_count', 'off')
     print(removepunc)
     print(djtxt)
+
     if(removepunc == 'on'):  # Check if checkbox is checked
         punctuation_list = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
         analyzer = ""
@@ -32,7 +35,17 @@ def analyze(request):
                 analyzer = analyzer + char
         params = {'purpose':'new_line_remover', 'analyzed_txt': analyzer}
 
+    elif(extra_space_remover == 'on'):
+        analyzer = ""
+        for index,char in enumerate(djtxt):
+            if not (djtxt[index] == "" and djtxt[index+1] == ""):
+                analyzer = analyzer + char
+
+    elif (char_count == 'on'):
+        analyzer = str(len(djtxt))  # Calculate the length of the text
+        params = {'purpose': 'Character Count', 'analyzed_txt': analyzer}
+
     else:
-        params = {'purpose': 'No Operation Selected', 'analyzed_txt': djtxt}
+        params = {'purpose': 'No Operation Selected','analyzed_txt': djtxt}
 
     return render(request, 'analyze.html', params)
