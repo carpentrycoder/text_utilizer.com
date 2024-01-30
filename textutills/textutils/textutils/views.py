65d005a1891cb2f2d2abb522cc6e56b2b23a51bd
Sutar <nikhilsutar621@gmail.com>
2024-01-30 20:27:41 +0530

    intial updates and adding CSRF token
    - CSRF token for data security

--- textutills/textutils/textutils/views.py
+++ textutills/textutils/textutils/views.py
@@ -1,3 +1,4 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,25 +6,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-    # return HttpResponse("Home")
-
-
-def ex1(request):
-    sites = ['''<h1>For Entertainment  </h1> <a href="https://www.youtube.com/"> Youtube Videos</a> ''',
-             '''<h1>For Interaction  </h1> <a href="https://www.facebook.com/"> Facebook</a> ''',
-             '''<h1>For Insight  </h1> <a href="https://www.ted.com/talks"> Ted Talks</a> ''',
-             '''<h1>For Internship  </h1> <a href="https://www.internshala.com">Internship</a> ''']
-    return HttpResponse((sites))
 
 def analyze(request):
     #Get the text
-    djtext = request.GET.get('text', 'default')
+    djtext = request.POST.get('text', 'default')
 
     # Check checkbox values
-    removepunc = request.GET.get('removepunc', 'off')
-    fullcaps = request.GET.get('fullcaps', 'off')
-    newlineremover = request.GET.get('newlineremover', 'off')
-    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
 
     #Check which checkbox is on
     if removepunc == "on":
@@ -32,36 +24,36 @@ def analyze(request):
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
 
-    elif(fullcaps=="on"):
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(fullcaps=="on"):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
 
-    elif(extraspaceremover=="on"):
+    if(extraspaceremover=="on"):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not(djtext[index] == " " and djtext[index+1]==" "):
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
 
-    elif (newlineremover == "on"):
+    if (newlineremover == "on"):
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char!="\r":
                 analyzed = analyzed + char
 
         params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
-        # Analyze the text
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
